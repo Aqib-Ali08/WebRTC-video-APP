ea8e5856d46c44477c73997349eb7ecce453538f
Sk Aqib Ali <122338420+Aqib-Ali08@users.noreply.github.com>
2024-11-30 21:53:45 +0530

    changed link

--- src/components/meetings.jsx
+++ src/components/meetings.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import '../assets/meeting.css';
 import meetingBGImg from '../assets/metting1.png';
 
-const socket = io('http://localhost:5000'); // Adjust for your server
+const socket = io('https://webrtc-backend-vtyh.onrender.com'); // Adjust for your server
 
 const MeetingPage = () => {
   const [currentTime, setCurrentTime] = useState('');
